bd86761bf26ebcb681dbe9b32988644b3d556255
osc1blog <osc1.blog@gmail.com>
2024-05-03 00:18:04 +0200

    Replace old version with a new draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ["./**/*.{razor,html,cshtml}"],
+  content: ["./**/*.{razor,html,cshtml}", "./wwwroot/css/app.css"],
   theme: { extend: {} },
   plugins: [
-
-  ]
+    require('daisyui')
+  ],
+  daisyui: {
+    base: false,
+  },
 }
