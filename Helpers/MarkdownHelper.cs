3381e0d9bcd2c29ed4f6fbf6cfe134f703abb184
osc1blog <osc1.blog@gmail.com>
2024-05-04 00:39:44 +0200

    Initial implementation for reading frontmatter from a post

--- Helpers/MarkdownHelper.cs
+++ Helpers/MarkdownHelper.cs
@@ -1,3 +1,8 @@
+using Markdig;
+using Markdig.Extensions.Yaml;
+using Markdig.Syntax;
+using YamlDotNet.Serialization;
+
 namespace BlazorPortfolio.Helpers {
     public static class MarkdownHelper
     {
---
+++ Models/Post.cs
@@ -0,0 +1,18 @@
+
+namespace BlazorPortfolio.Models {
+    public class Post
+    {
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public DateOnly Date { get; set; }
+        
+        Post(string fileName) {
+            FileName = fileName;
+
+        }
+
+        //todo: method to get everything you need from filename
+        //like frontmatter,  but not the content because thats memory heavy
+        //and should only happen in oninitiailzed in Post page
+    }
+}
