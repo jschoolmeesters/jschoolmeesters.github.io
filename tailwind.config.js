d9d89faacf9f2722f890babb9fda0ba5d63ca40a
osc1blog <osc1.blog@gmail.com>
2024-05-08 19:16:32 +0200

    Fix Tailwind and DaisyUI config and new style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 module.exports = {
-  content: ["./**/*.{razor,html,cshtml}", "./wwwroot/css/app.css"],
+  content: ["./wwwroot/index.html", "./Pages/*.{razor,html,cshtml}"],
   theme: { extend: {} },
   plugins: [
-    require('daisyui')
+    require('daisyui'),
+    require('tailwindcss'),
   ],
   daisyui: {
-    base: false,
+    themes: [
+      {
+        mytheme: {
+          "primary": "#669900",
+          "secondary": "#998000",
+          "accent": "#330099",
+          "neutral": "#3b3b3b",
+          "base-100": "#141612",
+          "info": "#006699",
+          "success": "#1a9900",
+          "warning": "#998000",
+          "error": "#993300",
+        },
+      },
+    ],
   },
 }
