88f5329a3edd4185cbaae0a138848622ec7a322f
osc1blog <osc1.blog@gmail.com>
2024-06-02 19:27:26 +0200

    make it blackkk

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,20 +7,13 @@ module.exports = {
     require('@tailwindcss/typography'),
   ],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "#669900",
-          "secondary": "#998000",
-          "accent": "#330099",
-          "neutral": "#3b3b3b",
-          "base-100": "#141612",
-          "info": "#006699",
-          "success": "#1a9900",
-          "warning": "#998000",
-          "error": "#993300",
-        },
-      },
-    ],
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    //darkTheme: "dark", // name of one of the included themes for dark mode
+    base: false, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
   },
 }
