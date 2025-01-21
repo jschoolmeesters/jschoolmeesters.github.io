0dff83ae034d363451e8e4d33029561be4503aa9
osc1blog <osc1.blog@gmail.com>
2025-01-21 17:50:30 +0100

    Create contact page

---
+++ Models/MailForm.cs
@@ -0,0 +1,8 @@
+public class MailForm
+{
+    public string FullName { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Budget { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
